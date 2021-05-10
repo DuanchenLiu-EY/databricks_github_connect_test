edbec0360b6bad36808e81cf0ca9d4a7a8b67207
DuanchenLiu-EY <83933545+DuanchenLiu-EY@users.noreply.github.com>
2021-05-10 11:26:58 -0400

    add line to main branch, after new branch creation

--- notebooks/Users/duanchen.liu@ctpsandbox.com/test_github_version_control/github_connect_test.py
+++ notebooks/Users/duanchen.liu@ctpsandbox.com/test_github_version_control/github_connect_test.py
@@ -1,2 +1,10 @@
 # Databricks notebook source
 print("hello world")
+
+# COMMAND ----------
+
+print("add new stuff in new-branch")
+
+# COMMAND ----------
+
+print("add another line to main branch")
